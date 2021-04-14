0babd7f82b62018914cd9eef1058303dcce9a98f
ghostNote <selcukbilgen@hotmail.com>
2021-04-14 22:26:48 +0300

    table style created

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,37 @@
 const { guessProductionMode } = require("@ngneat/tailwind");
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
-process.env.TAILWIND_MODE = guessProductionMode() ? 'build' : 'watch';
+process.env.TAILWIND_MODE = guessProductionMode() ? "build" : "watch";
 
 module.exports = {
-    prefix: '',
-    mode: 'jit',
-    purge: {
-      content: [
-        './src/**/*.{html,ts,css,scss,sass,less,styl}',
-      ]
+  prefix: "",
+  mode: "jit",
+  purge: {
+    content: ["./src/**/*.{html,ts,css,scss,sass,less,styl}"],
+  },
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    colors: {
+      // Build your palette here
+      // transparent: "transparent",
+      // current: "currentColor",
+      white: colors.white,
+      blueGray: colors.blueGray,
+      green: colors.green,
+      lightBlue: colors.lightBlue,
+      teal: colors.teal,
+      emerald: colors.emerald,
+      yellow: colors.yellow,
+      amber: colors.amber,
+      orange: colors.orange,
+      red: colors.red,
+      rose: colors.rose,
+      pink: colors.pink,
     },
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [require('@tailwindcss/forms'),require('@tailwindcss/typography')],
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
